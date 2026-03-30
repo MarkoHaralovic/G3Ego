e8338ddfd91067663ab50f7524ba94d6854aee39
MarkoHaralovic <mharalovic11@gmail.com>
2026-03-30 14:47:21 +0200

    upadted requirements for meccano, slowfast and optionally detectron, added VLM annotation with QWEN3-VL, modified for MECCANO dataset

--- slurm_scripts/eval_pretrained_model.sh
+++ slurm_scripts/eval_pretrained_model.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=slowfast_eval
 #SBATCH --time=10:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --output /home/s3758869/egocentric_video_graph_framework_ar/SlowFast/sbatch_scripts/outputs/slowfast_eval%J.log
+#SBATCH --output /home/s3758869/egocentric_video_graph_framework_ar/slurm_outputs/slowfast_eval%J.log
 
 cd /home/s3758869
 source  source /aria_env/bin/activate
